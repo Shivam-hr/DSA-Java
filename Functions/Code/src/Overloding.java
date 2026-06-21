9462890af61d25f48ece45fce16880ef5c718b35
Shivam-hr <Shivanhrsalwan@gmail.com>
2026-06-21 20:08:06 +0530

    learrn Overloding

--- Functions/Code/src/Overloding.java
+++ Functions/Code/src/Overloding.java
@@ -1,2 +1,21 @@
 public class Overloding {
+
+    public static void main(String[] args) {
+
+    fun(10, 20);
+    }
+//    same name fxn can be exist if they have diff datatype and
+//    even if have same datatype but they have to be diff numbers of argument
+
+    static void fun(int a, int b){
+
+    }
+
+    static void fun(String a){
+
+    }
+
+    static void fun(String a, String b){
+
+    }
 }
