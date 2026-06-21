7d95a116cd41eba11fac4b5be1222e30db3484a9
Shivam-hr <Shivanhrsalwan@gmail.com>
2026-06-21 20:12:54 +0530

    learn Overloading

--- Functions/Code/src/Overloding.java
+++ Functions/Code/src/Overloding.java
@@ -3,6 +3,7 @@ public class Overloding {
     public static void main(String[] args) {
 
     fun(10, 20);
+    fun("Shivm");
     }
 //    same name fxn can be exist if they have diff datatype and
 //    even if have same datatype but they have to be diff numbers of argument
